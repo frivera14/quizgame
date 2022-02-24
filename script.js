748191d51ab27d657590e2798d802432c556d409
unknown <kikridu@gmail.com>
2022-02-24 11:57:21 -0700

    added variables and planned function

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
 var startBtn = document.getElementById('start-quiz')
 var header = document.getElementById('welcome');
-
+var mkList = document.createElement("ul");
+var mkItem = document.createElement("li")
+var optBtn = document.createElement("button");
+var option1 = false
+var option2 = false
+var option3 = false
+var option4 = false
 
 function countdown() {
   var timeLeft = 20;
@@ -24,6 +30,12 @@ function countdown() {
     }
   }, 1000);
 };
+
+// function startQuiz()
+// change header to display question
+// append li buttons 
+// add true value to prompt correct
+// add false value to prompt wrong answer and deduct x seconds from timer
 
 
 startBtn.addEventListener("click", countdown)
