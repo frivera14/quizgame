7c89477f8b8d5284a0bd2da137d753799adc6efb
unknown <kikridu@gmail.com>
2022-02-23 22:47:50 -0700

    added button and timer

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('main');
+var startBtn = document.getElementById('start-quiz')
+var header = document.getElementById('welcome');
 
 
 function countdown() {
-  var timeLeft = 5;
+  var timeLeft = 20;
+  startBtn.remove();
+  header.innerHTML = '';
 
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function() {
+
     if (timeLeft > 1) {
-      timerEl.textContent = timeLeft + ' seconds remaining';
+      timerEl.textContent = timeLeft;
       timeLeft--;
     }else if (timeLeft === 1) {
-      timerEl.textContent = timeLeft + ' second remaining';
+      timerEl.textContent = timeLeft;
       timeLeft--;
     }else {
       timerEl.textContent = ' ';
       clearInterval();
-      displayMessage();
     }
   }, 1000);
 };
 
-countdown();
 
-function displayMessage() {
-    ;
-}
+startBtn.addEventListener("click", countdown)
